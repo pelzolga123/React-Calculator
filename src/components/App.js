a77c0f89e0c7e4e79ac4af3edf000af651ae2a4c
pelzolga123 <pelzolga123@gmail.com>
2020-01-23 00:32:02 +0200

    Finished current milestone

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,14 @@
-import React, { Component } from 'react';
-import ButtonPanel from './ButtonPanel';
+import React from 'react';
+import { ButtonPanel } from './ButtonPanel';
 import Display from './Display';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Calculator</h1>
-        <Display />
-        <ButtonPanel />
-      </div>
-    );
-  }
-}
+export const App = () => (
+  <div className="App">
+    <h1>Calculator</h1>
+    <Display />
+    <ButtonPanel />
+  </div>
+);
 
 export default App;
