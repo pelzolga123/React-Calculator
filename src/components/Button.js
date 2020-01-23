8bb2434f1bb1fc7c42ac60257eb3ec9840cda1c0
pelzolga123 <pelzolga123@gmail.com>
2020-01-24 00:27:41 +0200

    Added boolean width parameter

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,18 +2,32 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Button = (props) => {
-  const { btnName } = props;
+  const { btnName, color, wide } = props;
+  let size = wide;
+  if (wide) {
+    size = '50%';
+  } else size = '25%';
+
   return (
-    <button type="button">{btnName}</button>
+    <button
+      type="button"
+      style={{ backgroundColor: color, width: size }}
+    >
+      {btnName}
+    </button>
   );
 };
 
 Button.propTypes = {
   btnName: PropTypes.string,
+  color: PropTypes.string,
+  wide: PropTypes.bool,
 };
 
 Button.defaultProps = {
   btnName: '',
+  color: '#FF8C00',
+  wide: false,
 };
 
 export default Button;
