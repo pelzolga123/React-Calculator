4a45fb122149a0e55f5dace11ec4512883102bce
pelzolga123 <pelzolga123@gmail.com>
2020-01-23 12:36:07 +0200

    Fixed linter errors

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Button = (name) => (
-  <button type="button">{name}</button>
+const Button = (props) => {
+  const { name } = props;
+  return (
+    <button type="button">{name}</button>
+  );
+};
 
-);
+Button.defaultProps = {
+  name: '',
+};
 
 Button.propTypes = {
   name: PropTypes.string,
