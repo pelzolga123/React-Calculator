b90478737192918cdbf01e99989e2b5ef5f58118
pelzolga123 <pelzolga123@gmail.com>
2020-01-24 15:55:26 +0200

    Added few [optional] changes

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,31 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Button = (props) => {
-  const { btnName, color, wide } = props;
-  let size = wide;
-  if (wide) {
-    size = '50%';
-  } else size = '25%';
-
-  return (
-    <button
-      type="button"
-      style={{ backgroundColor: color, width: size }}
-    >
-      {btnName}
-    </button>
-  );
-};
+const Button = ({ btnName, color, wide }) => (
+  <button
+    type="button"
+    style={{ backgroundColor: color, width: wide ? '50%' : '25%' }}
+  >
+    {btnName}
+  </button>
+);
 
 Button.propTypes = {
-  btnName: PropTypes.string,
+  btnName: PropTypes.string.isRequired,
   color: PropTypes.string,
   wide: PropTypes.bool,
 };
 
 Button.defaultProps = {
-  btnName: '',
   color: '#FF8C00',
   wide: false,
 };
